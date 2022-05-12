6aad86e631d6257e6ed24348c21bf61c4bfcfca9
HudsonHawk88 <toth.gergo88@hotmail.com>
2022-05-12 23:00:37 +0200

    fix(react-data-table): rewrite in typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+/* import React from 'react';
+import { createRoot } from 'react-dom/client'; */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-import datas from './datas'
-/* import columns from './columns'; */
+/* import datas from './datas'
+import columns from './columns'; */
 import DataTable from './DataTable';
 
-const root = createRoot(document.getElementById('root'));
+/* const root = createRoot(document.getElementById('root'));
 
-const columns = [
-    {
-        dataField: 'name',
-        text: 'Név',
-        filter: true,
-        filterType: 'textFilter'
-    },
-    {
-        dataField: 'occupation',
-        text: 'Foglalkozás',
-        filter: true,
-        /* filterType: 'text' */
-        filterType: 'optionFilter',
-        filterOptions: [
-            {
-                id: 'frontend developer',
-                value: 'frontend developer',
-                text: 'frontend developer'
-            },
-            {
-                id: 'backend developer',
-                value: 'backend developer',
-                text: 'backend developer'
-            }
-        ]
-    },
-    {
-        dataField: 'id',
-        text: 'Műveletek',
-        formatter: (cell: any, row: any) => {
-            return <React.Fragment><button>{row.id}</button>&nbsp;&nbsp;<button>{row.id}</button>&nbsp;&nbsp;<button>{row.id}</button></React.Fragment>
-        }
-    }
-];
 
 const paginationOptions = {
     color: 'primary', count: 5, nextText: '>', previousText: '<', firstPageText: '<<', lastPageText: '>>', rowPerPageOptions: [
@@ -60,7 +26,7 @@ const paginationOptions = {
         },
     ]
 }
-root.render(<DataTable bordered datas={datas} columns={columns} paginationOptions={paginationOptions} />);
+root.render(<DataTable bordered datas={datas} columns={columns} paginationOptions={paginationOptions} />); */
 
 
 export { DataTable }
