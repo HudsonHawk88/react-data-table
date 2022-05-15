ebcda70e7fd5b23a0006490aabe3ffb5a82d5a69
HudsonHawk88 <toth.gergo88@hotmail.com>
2022-05-15 22:53:54 +0200

    fix(react-data-table): fix importing and exporting

--- dist/DataTable.js
+++ dist/DataTable.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
@@ -23,64 +34,67 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importStar(require("react"));
-const reactstrap_1 = require("reactstrap");
+var react_1 = __importStar(require("react"));
+var reactstrap_1 = require("reactstrap");
 require("./styles/index.css");
 require("bootstrap/dist/css/bootstrap.min.css");
-const DataTable = ({ className = 'react-data-table', datas, columns, paginationOptions, bordered = false, striped = false }) => {
-    const [filters, setFilters] = (0, react_1.useState)({});
-    const [filtered, setFiltered] = (0, react_1.useState)([]);
-    const [count, setCount] = (0, react_1.useState)(paginationOptions && paginationOptions.count ? paginationOptions.count : 5);
-    const [currentPage, setCurrentPage] = (0, react_1.useState)(0);
-    const [pageButtons, setPageButtons] = (0, react_1.useState)([]);
-    (0, react_1.useEffect)(() => {
+var DataTable = function (_a) {
+    var _b = _a.className, className = _b === void 0 ? 'react-data-table' : _b, datas = _a.datas, columns = _a.columns, paginationOptions = _a.paginationOptions, _c = _a.bordered, bordered = _c === void 0 ? false : _c, _d = _a.striped, striped = _d === void 0 ? false : _d;
+    var _e = (0, react_1.useState)({}), filters = _e[0], setFilters = _e[1];
+    var _f = (0, react_1.useState)([]), filtered = _f[0], setFiltered = _f[1];
+    var _g = (0, react_1.useState)(paginationOptions && paginationOptions.count ? paginationOptions.count : 5), count = _g[0], setCount = _g[1];
+    var _h = (0, react_1.useState)(0), currentPage = _h[0], setCurrentPage = _h[1];
+    var _j = (0, react_1.useState)([]), pageButtons = _j[0], setPageButtons = _j[1];
+    (0, react_1.useEffect)(function () {
         setFiltered(datas);
         /* if (paginationOptions) {
             setCount(paginationOptions.count || 5);
         } */
     }, [datas]);
-    const setDefaultFilters = (0, react_1.useCallback)(() => {
-        const cols = columns || [];
-        let filterObj = {};
-        cols.forEach((col) => {
+    var setDefaultFilters = (0, react_1.useCallback)(function () {
+        var cols = columns || [];
+        var filterObj = {};
+        cols.forEach(function (col) {
+            var _a, _b;
             if (col.filter) {
                 if (col.filterType === 'textFilter') {
-                    Object.assign(filterObj, { [col.dataField]: '' });
+                    Object.assign(filterObj, (_a = {}, _a[col.dataField] = '', _a));
                 }
                 else if (col.filterType === 'optionFilter') {
-                    Object.assign(filterObj, { [col.dataField]: '' });
+                    Object.assign(filterObj, (_b = {}, _b[col.dataField] = '', _b));
                 }
             }
         });
         setFilters(filterObj);
     }, [columns]);
-    (0, react_1.useEffect)(() => {
+    (0, react_1.useEffect)(function () {
         setDefaultFilters();
     }, [setDefaultFilters]);
-    const handleFilterChange = (e) => {
-        const { target } = e;
-        const { name, value } = target;
-        setFilters(Object.assign(Object.assign({}, filters), { [name]: value }));
+    var handleFilterChange = function (e) {
+        var _a;
+        var target = e.target;
+        var name = target.name, value = target.value;
+        setFilters(__assign(__assign({}, filters), (_a = {}, _a[name] = value, _a)));
     };
-    const getFilter = (col) => {
-        const filterType = col.filterType;
+    var getFilter = function (col) {
+        var filterType = col.filterType;
         switch (filterType) {
             case 'textFilter': {
-                const defaultValue = col.defaultValue || '';
+                var defaultValue = col.defaultValue || '';
                 return (react_1.default.createElement("th", { key: col.text },
                     col.text,
                     react_1.default.createElement("br", null),
                     react_1.default.createElement(reactstrap_1.Input, { name: col.dataField, type: 'text', placeholder: defaultValue, value: filters[col.dataField], onChange: handleFilterChange })));
             }
             case 'optionFilter': {
-                const filterOptions = col.filterOptions || [];
-                const defaultValue = col.defaultValue || 'Kérjük válasszon...';
+                var filterOptions = col.filterOptions || [];
+                var defaultValue = col.defaultValue || 'Kérjük válasszon...';
                 return (react_1.default.createElement("th", { key: col.text },
                     col.text,
                     react_1.default.createElement("br", null),
                     react_1.default.createElement(reactstrap_1.Input, { name: col.dataField, type: 'select', onChange: handleFilterChange },
                         react_1.default.createElement("option", { key: 'filter_' + filterOptions.id, value: '' }, defaultValue),
-                        filterOptions.map((filterOption) => {
+                        filterOptions.map(function (filterOption) {
                             return react_1.default.createElement("option", { key: 'filter_' + filterOption.id, value: filterOption.value }, filterOption.text);
                         }))));
             }
@@ -89,14 +103,14 @@ const DataTable = ({ className = 'react-data-table', datas, columns, paginationO
             }
         }
     };
-    const renderHeaderCells = () => {
-        const cols = columns || [];
-        return (react_1.default.createElement("tr", null, cols.map((col) => {
+    var renderHeaderCells = function () {
+        var cols = columns || [];
+        return (react_1.default.createElement("tr", null, cols.map(function (col) {
             return !col.hidden && getFilter(col);
         })));
     };
-    const getFilterClause = (0, react_1.useCallback)((key, rowData) => {
-        const ccc = columns.find((c) => c['dataField'] === key);
+    var getFilterClause = (0, react_1.useCallback)(function (key, rowData) {
+        var ccc = columns.find(function (c) { return c['dataField'] === key; });
         if (ccc && ccc.filterType === 'textFilter') {
             return rowData[key].toLowerCase().indexOf(filters[key].toLowerCase()) === -1;
         }
@@ -107,11 +121,11 @@ const DataTable = ({ className = 'react-data-table', datas, columns, paginationO
             return undefined;
         }
     }, [columns, filters]);
-    const filteringData = (0, react_1.useCallback)(() => {
-        let newDatas = datas;
-        newDatas = datas.filter((rowData) => {
-            let res = true;
-            for (let key in filters) {
+    var filteringData = (0, react_1.useCallback)(function () {
+        var newDatas = datas;
+        newDatas = datas.filter(function (rowData) {
+            var res = true;
+            for (var key in filters) {
                 if (filters[key] !== '') {
                     if (getFilterClause(key, rowData)) {
                         res = false;
@@ -122,46 +136,49 @@ const DataTable = ({ className = 'react-data-table', datas, columns, paginationO
         });
         return newDatas;
     }, [filters, datas, getFilterClause]);
-    const getFirstIndex = (0, react_1.useCallback)(() => {
-        const cP = currentPage;
-        const firstIndex = ((cP - 2) < 0) ? 0 : (cP - 2);
+    var getFirstIndex = (0, react_1.useCallback)(function () {
+        var cP = currentPage;
+        var firstIndex = ((cP - 2) < 0) ? 0 : (cP - 2);
         return firstIndex;
     }, [currentPage]);
-    const getLastIndex = (0, react_1.useCallback)(() => {
-        const pageCount = pageButtons.length;
-        const cP = currentPage;
-        const lastIndex = ((cP + 3) > pageCount) ? pageCount : (cP + 3);
+    var getLastIndex = (0, react_1.useCallback)(function () {
+        var pageCount = pageButtons.length;
+        var cP = currentPage;
+        var lastIndex = ((cP + 3) > pageCount) ? pageCount : (cP + 3);
         return lastIndex;
     }, [currentPage, pageButtons.length]);
-    const createPageButtons = (0, react_1.useCallback)((filteredData) => {
-        const length = filteredData.length;
-        const pageCount = Math.ceil(length / count);
-        let pageButtonsArray = [];
-        for (let i = 0; i < pageCount; i++) {
-            pageButtonsArray.push({ key: i, onClick: () => onPageClick(i), text: i + 1 });
+    var createPageButtons = (0, react_1.useCallback)(function (filteredData) {
+        var length = filteredData.length;
+        var pageCount = Math.ceil(length / count);
+        var pageButtonsArray = [];
+        var _loop_1 = function (i) {
+            pageButtonsArray.push({ key: i, onClick: function () { return onPageClick(i); }, text: i + 1 });
+        };
+        for (var i = 0; i < pageCount; i++) {
+            _loop_1(i);
         }
         setPageButtons(pageButtonsArray);
     }, [count]);
-    const handlePageChange = (0, react_1.useCallback)((filteredData) => {
-        const firstIndex = count * currentPage;
-        const lastIndex = firstIndex + count;
-        let filteredArray = [];
-        for (let i = firstIndex; i < lastIndex; i++) {
+    var handlePageChange = (0, react_1.useCallback)(function (filteredData) {
+        var firstIndex = count * currentPage;
+        var lastIndex = firstIndex + count;
+        var filteredArray = [];
+        for (var i = firstIndex; i < lastIndex; i++) {
             if (filteredData[i]) {
                 filteredArray.push(filteredData[i]);
             }
         }
         setFiltered(filteredArray);
     }, [count, currentPage]);
-    (0, react_1.useMemo)(() => {
-        const filteredData = filteringData();
+    (0, react_1.useMemo)(function () {
+        var filteredData = filteringData();
         handlePageChange(filteredData);
         createPageButtons(filteredData);
     }, [filteringData, handlePageChange, createPageButtons]);
-    const renderCell = (col, row) => {
-        const { formatter, hidden } = col;
-        const name = col['dataField'];
-        let cell = '';
+    var renderCell = function (col, row) {
+        var formatter = col.formatter, hidden = col.hidden;
+        var name = col['dataField'];
+        var cell = '';
         if (col && row && !hidden) {
             if (formatter && typeof formatter === 'function') {
                 return react_1.default.createElement("td", { key: 'cell_' + row.id }, col.formatter(col, row));
@@ -172,69 +189,69 @@ const DataTable = ({ className = 'react-data-table', datas, columns, paginationO
         }
         return cell;
     };
-    const renderCells = (r) => {
-        const row = r || {};
-        const cols = columns || [];
-        return (cols.map((col) => {
+    var renderCells = function (r) {
+        var row = r || {};
+        var cols = columns || [];
+        return (cols.map(function (col) {
             return renderCell(col, row);
         }));
     };
-    const renderTable = () => {
-        const rows = filtered || [];
+    var renderTable = function () {
+        var rows = filtered || [];
         return (react_1.default.createElement(reactstrap_1.Table, { className: className, striped: striped ? striped : false, bordered: bordered ? bordered : false },
             react_1.default.createElement("thead", null, renderHeaderCells()),
-            react_1.default.createElement("tbody", null, rows.map((row) => {
+            react_1.default.createElement("tbody", null, rows.map(function (row) {
                 return (react_1.default.createElement("tr", { key: 'row_' + row.id }, renderCells(row)));
             }))));
     };
-    const onNextClick = () => {
+    var onNextClick = function () {
         setCurrentPage(currentPage + 1);
     };
-    const onPreviousClick = () => {
+    var onPreviousClick = function () {
         setCurrentPage(currentPage - 1);
     };
-    const onFirstClick = () => {
+    var onFirstClick = function () {
         setCurrentPage(0);
     };
-    const onLastClick = () => {
+    var onLastClick = function () {
         setCurrentPage(pageButtons.length - 1);
     };
-    const onPageClick = (pageNumber) => {
+    var onPageClick = function (pageNumber) {
         setCurrentPage(pageNumber);
     };
-    const renderRowPerPage = (rowPerPageOptions) => {
-        const defaultRowPerPageStyle = {
+    var renderRowPerPage = function (rowPerPageOptions) {
+        var defaultRowPerPageStyle = {
             width: 'fit-content',
             float: 'left'
         };
-        return (react_1.default.createElement(reactstrap_1.Input, { className: 'react-data-table-pagination-rowperpage', style: defaultRowPerPageStyle, type: 'select', value: count, onChange: (e) => { setCount(parseInt(e.target.value, 10)); setCurrentPage(0); } }, rowPerPageOptions && Array.isArray(rowPerPageOptions) && rowPerPageOptions.length > 0 && (rowPerPageOptions.map((opt) => {
-            const { value, text } = opt;
+        return (react_1.default.createElement(reactstrap_1.Input, { className: 'react-data-table-pagination-rowperpage', style: defaultRowPerPageStyle, type: 'select', value: count, onChange: function (e) { setCount(parseInt(e.target.value, 10)); setCurrentPage(0); } }, rowPerPageOptions && Array.isArray(rowPerPageOptions) && rowPerPageOptions.length > 0 && (rowPerPageOptions.map(function (opt) {
+            var value = opt.value, text = opt.text;
             return (react_1.default.createElement("option", { key: value, value: value }, text));
         }))));
     };
-    const renderPageButtons = (color, nextText, previousText, firstPageText, lastPageText) => {
-        const defaultPageButtonsStyle = {
+    var renderPageButtons = function (color, nextText, previousText, firstPageText, lastPageText) {
+        var defaultPageButtonsStyle = {
             float: 'right'
         };
-        const firstIndex = getFirstIndex();
-        const lastIndex = getLastIndex();
-        let buttons = [];
-        for (let i = firstIndex; i < lastIndex; i++) {
+        var firstIndex = getFirstIndex();
+        var lastIndex = getLastIndex();
+        var buttons = [];
+        for (var i = firstIndex; i < lastIndex; i++) {
             buttons.push(react_1.default.createElement(reactstrap_1.Button, { color: color, outline: currentPage !== pageButtons[i].key, key: pageButtons[i].key, onClick: pageButtons[i].onClick }, pageButtons[i].text));
         }
         return (react_1.default.createElement("div", { className: 'react-data-table-pagination-buttons', style: defaultPageButtonsStyle },
-            react_1.default.createElement(reactstrap_1.Button, { outline: true, color: color, hidden: currentPage === 0, onClick: () => onFirstClick() }, firstPageText),
-            react_1.default.createElement(reactstrap_1.Button, { outline: true, color: color, hidden: currentPage === 0, onClick: () => onPreviousClick() }, previousText),
-            buttons.map((butt) => {
+            react_1.default.createElement(reactstrap_1.Button, { outline: true, color: color, hidden: currentPage === 0, onClick: function () { return onFirstClick(); } }, firstPageText),
+            react_1.default.createElement(reactstrap_1.Button, { outline: true, color: color, hidden: currentPage === 0, onClick: function () { return onPreviousClick(); } }, previousText),
+            buttons.map(function (butt) {
                 return butt;
             }),
-            react_1.default.createElement(reactstrap_1.Button, { outline: true, color: color, hidden: currentPage === pageButtons.length - 1, onClick: () => onNextClick() }, nextText),
-            react_1.default.createElement(reactstrap_1.Button, { outline: true, color: color, hidden: currentPage === pageButtons.length - 1, onClick: () => onLastClick() }, lastPageText)));
+            react_1.default.createElement(reactstrap_1.Button, { outline: true, color: color, hidden: currentPage === pageButtons.length - 1, onClick: function () { return onNextClick(); } }, nextText),
+            react_1.default.createElement(reactstrap_1.Button, { outline: true, color: color, hidden: currentPage === pageButtons.length - 1, onClick: function () { return onLastClick(); } }, lastPageText)));
     };
-    const renderPagination = () => {
+    var renderPagination = function () {
         if (paginationOptions && typeof paginationOptions === 'object' && filtered.length > 0) {
-            const { color, nextText = '>', previousText = '<', firstPageText = '<<', lastPageText = '>>', rowPerPageOptions } = paginationOptions;
-            const defaultPaginationStyle = {
+            var color = paginationOptions.color, _a = paginationOptions.nextText, nextText = _a === void 0 ? '>' : _a, _b = paginationOptions.previousText, previousText = _b === void 0 ? '<' : _b, _c = paginationOptions.firstPageText, firstPageText = _c === void 0 ? '<<' : _c, _d = paginationOptions.lastPageText, lastPageText = _d === void 0 ? '>>' : _d, rowPerPageOptions = paginationOptions.rowPerPageOptions;
+            var defaultPaginationStyle = {
                 width: '100%',
                 clear: 'both'
             };
